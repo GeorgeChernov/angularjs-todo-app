91365c5e21dcf31b744ae513441304c7bdf92a24
George Chernov <chernovgosha@gmail.com>
2018-08-19 22:03:19 +0300

    Add the unit test to check DOM of Task component

--- tests/components/task.spec.js
+++ tests/components/task.spec.js
@@ -1,19 +1,46 @@
 describe('task component', function () {
 
-    var $componentController;
-
     beforeEach(module('app'));
-    beforeEach(inject(function (_$componentController_) {
-        $componentController = _$componentController_;
-    }));
 
-    it('should has name property with value from bindings', function () {
-        var bindings = {
-            name: "task name"
-        };
+    describe('controller', function () {
+        var $componentController;
 
-        var ctrl = $componentController('task', null, bindings);
+        beforeEach(inject(function (_$componentController_) {
+            $componentController = _$componentController_;
+        }));
 
-        expect(ctrl.name).toBe('task name');
-    })
+        it('should has name property with value from bindings', function () {
+            var bindings = {
+                name: "task name"
+            };
+
+            var ctrl = $componentController('task', null, bindings);
+
+            expect(ctrl.name).toBe('task name');
+        })
+    });
+
+    describe('dom', function () {
+        let $compile;
+        let scope;
+
+        beforeEach(inject(($rootScope, _$compile_) => {
+            scope = $rootScope.$new();
+            $compile = _$compile_;
+        }));
+
+        it('should display correct name', () => {
+
+            // prepare scope
+            scope['name'] = 'Test task';
+
+            // render the element
+            let element = angular.element(`<task name="{{name}}"></task>`);
+            element = $compile(element)(scope);
+            scope.$digest();
+
+            // test the element
+            expect(element.text().trim()).toEqual('Test task');
+        });
+    });
 });
